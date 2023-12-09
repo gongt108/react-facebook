c49af90ab1a77aaf333bd551c8071b0780db905b
Tiffany Gong <gongt108@gmail.com>
2023-12-09 12:19:54 -0800

    feat: complete gaming page

--- pages/components/large-game-card.js
+++ pages/components/large-game-card.js
@@ -3,10 +3,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../../styles/Gaming.module.css';
 
-function SmallGameCard({ img, name, playersNum }) {
+function LargeGameCard({ img, name, playersNum }) {
 	return (
-		<div className={styles.smallGameCard}>
-			<Image src={img} width="150" height="150"></Image>
+		<div className={styles.largeGameCard}>
+			<Image src={img} width="150" height="300"></Image>
 			<div className={styles.gameCardDetails}>
 				<h4>{name}</h4>
 				<p className={styles.gameCardPlayers}>{playersNum}</p>
@@ -15,4 +15,4 @@ function SmallGameCard({ img, name, playersNum }) {
 	);
 }
 
-export default SmallGameCard;
+export default LargeGameCard;
--- public/data/gaming.js
+++ public/data/gaming.js
@@ -2,28 +2,135 @@ const gamesWeLove = [
 	{
 		name: 'Yatzy Fever',
 		playersNum: '1M players',
-		img: 'yatzy.jpg',
+		img: '/games/yatzy.jpg',
 	},
 	{
 		name: 'Farmland',
 		playersNum: '125K players',
-		img: 'farmland.jpg',
+		img: '/games/farmland.jpg',
 	},
 	{
 		name: 'The Puzzle',
-		playersNum: '3M playeres',
-		img: 'the-puzzle.jpg',
+		playersNum: '3M players',
+		img: '/games/the-puzzle.jpg',
 	},
 	{
 		name: 'Drop 2048: Mission',
 		playersNum: '101K players',
-		img: 'drop-2048.jpg',
+		img: '/games/drop-2048.jpg',
 	},
 	{
-		name: 'Cooking center',
+		name: 'Cooking Center',
 		playersNum: '170K players',
-		img: 'cooking-center.jpg',
+		img: '/games/cooking-center.jpg',
 	},
 ];
 
-export default gamesWeLove;
+const mostPlayedLarge = [
+	{
+		name: 'SuperCity',
+		playersNum: '83K players',
+		img: '/games/supercity.png',
+	},
+];
+
+const mostPlayedSmall = [
+	{
+		name: 'Mahjong FRVR',
+		playersNum: '53K players',
+		img: '/games/mahjong.png',
+	},
+	{
+		name: 'Mini Golf',
+		playersNum: '17K players',
+		img: '/games/mini-golf.jpg',
+	},
+	{
+		name: 'Slot Mate',
+		playersNum: '149K players',
+		img: '/games/slot-mate.png',
+	},
+	{
+		name: 'Solitaire Home Story',
+		playersNum: '118K players',
+		img: '/games/solitaire-home-story.jpg',
+	},
+	{
+		name: 'Snake.io',
+		playersNum: '301K players',
+		img: '/games/snake.png',
+	},
+	{
+		name: 'Sudoku',
+		playersNum: '1M players',
+		img: '/games/sudoku.png',
+	},
+	{
+		name: 'Trivia Crack',
+		playersNum: '165K players',
+		img: '/games/trivia-crack.png',
+	},
+	{
+		name: 'The Puzzle',
+		playersNum: '3M players',
+		img: '/games/the-puzzle.jpg',
+	},
+];
+const topPicksLarge = [
+	{
+		name: '8 Ball Pool',
+		playersNum: '8M players',
+		img: '/games/8ball-pool.png',
+	},
+];
+
+const topPicksSmall = [
+	{
+		name: 'Animal Restaurant',
+		playersNum: '121K players',
+		img: '/games/animal-restaurant.jpg',
+	},
+	{
+		name: 'Basketball',
+		playersNum: '127K players',
+		img: '/games/basketball.jpg',
+	},
+	{
+		name: 'Bingo Holiday',
+		playersNum: '49K players',
+		img: '/games/the-puzzle.jpg',
+	},
+	{
+		name: 'Drop 2048: Mission',
+		playersNum: '101K players',
+		img: '/games/drop-2048.jpg',
+	},
+	{
+		name: 'Cooking Center',
+		playersNum: '170K players',
+		img: '/games/cooking-center.jpg',
+	},
+	{
+		name: 'Electric Slots',
+		playersNum: '312K players',
+		img: '/games/electric-slot.jpg',
+	},
+	{
+		name: 'Farmland',
+		playersNum: '126K players',
+		img: '/games/farmland.jpg',
+	},
+	{
+		name: 'Hero Wars',
+		playersNum: '77K players',
+		img: '/games/hero-wars.png',
+	},
+];
+
+export default {
+	gamesWeLove,
+	topPicksSmall,
+	topPicksLarge,
+	mostPlayedSmall,
+	mostPlayedLarge,
+};
